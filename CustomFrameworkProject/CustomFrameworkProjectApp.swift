7e15f643f0a42a0a59d1ce22baeda14d46ac31f9
Szymon Miękus <miekusoft@gmail.com>
2023-06-28 13:16:14 +0200

    Usage v0.0.1 & v0.0.2

--- CustomFrameworkProject/CustomFrameworkProjectApp.swift
+++ CustomFrameworkProject/CustomFrameworkProjectApp.swift
@@ -11,7 +11,14 @@ import CustomFramework
 @main
 struct CustomFrameworkProjectApp: App {
     
+//    // version 0.0.1
+//    init() {
+//        SDK.doSomeWork()
+//    }
+    
+    // version 0.0.2
     init() {
+        SDK.configure(apiKey: "secret")
         SDK.doSomeWork()
     }
     
